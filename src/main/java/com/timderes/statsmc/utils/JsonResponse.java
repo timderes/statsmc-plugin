03ba1f61c21506dac2a1e9c330ab55a79fe2438f
Tim Deres <50623315+timderes@users.noreply.github.com>
2025-10-23 23:39:24 +0200

    Add JsonResponse utility and Gson dependency
    Introduced a JsonResponse utility class for converting objects to JSON using Gson. Added Gson as a dependency in pom.xml to support JSON serialization.

---
+++ src/main/java/com/timderes/statsmc/utils/JsonResponse.java
@@ -0,0 +1,20 @@
+package com.timderes.statsmc.utils;
+
+import com.google.gson.Gson;
+
+/**
+ *
+ */
+public class JsonResponse {
+    private static final Gson GSON = new Gson();
+
+    /**
+     * Converts the input data to JSON format.
+     * 
+     * @param data The data to convert.
+     * @return A JSON string representation of the data.
+     */
+    public static String toJson(Object data) {
+        return GSON.toJson(data);
+    }
+}
