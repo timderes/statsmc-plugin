e0fd1369ed4529564d3deb3839bc4d71d042fb51
Tim Deres <50623315+timderes@users.noreply.github.com>
2025-10-27 16:49:01 +0100

    Update navbar links and active styling
    Changed Navbar.Brand to use React Router Link for proper navigation. Updated 'Home' link to 'Dashboard' and added activeProps for bold styling on active navigation links.

--- src/web/src/routes/__root.tsx
+++ src/web/src/routes/__root.tsx
@@ -12,14 +12,29 @@ function RootComponent() {
       <title>MC Stats</title>
       <Navbar expand="lg" className="bg-body-tertiary mb-4">
         <Container>
-          <Navbar.Brand href="#home">MC Stats</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">
+            MC Stats
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link as={Link} activeOptions={{ exact: true }} to="/">
-                Home
+              <Nav.Link
+                as={Link}
+                activeProps={{
+                  className: "fw-bold",
+                }}
+                activeOptions={{ exact: true }}
+                to="/"
+              >
+                Dashboard
               </Nav.Link>
-              <Nav.Link as={Link} to="/players">
+              <Nav.Link
+                as={Link}
+                activeProps={{
+                  className: "fw-bold",
+                }}
+                to="/players"
+              >
                 Players
               </Nav.Link>
             </Nav>
